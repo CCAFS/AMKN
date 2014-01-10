6715e73b9561cd7a4b2999e9f3f87290f280c950
camilor67 <camilor67@gmail.com>
2014-01-10 14:17:16 -0500

    header menu fixed
    fixed the menu highlighted, #33

--- wp-content/themes/amkn_theme/dataAndTools.php
+++ wp-content/themes/amkn_theme/dataAndTools.php
@@ -7,6 +7,17 @@ $http_respone_header = file_get_contents($url);
 $apps = json_decode($http_respone_header,true);
 //echo "<pre>".print_r( json_decode($http_respone_header,true),true)."</pre>"; 
 ?>
+<script>
+  document.getElementById("menu-item-2540").className =
+   document.getElementById("menu-item-2540").className.replace
+      ( /(?:^|\s)current-menu-item(?!\S)/ , '' );
+  document.getElementById("menu-item-846").className =
+   document.getElementById("menu-item-846").className.replace
+      ( /(?:^|\s)current-menu-item(?!\S)/ , '' );
+  document.getElementById("menu-item-846").className =
+   document.getElementById("menu-item-846").className.replace
+      ( /(?:^|\s)current_page_item(?!\S)/ , '' );
+</script>
 <div id="container">
 <h2>Data & Tools</h2>
 <?php
