3f2aa70b452d6d6971b746e232074d07270a31f6
camilor67 <camilor67@gmail.com>
2014-04-28 13:23:14 -0500

    Show a popup when click on an image

--- wp-content/themes/amkn_theme/ccafs-sites/photos.php
+++ wp-content/themes/amkn_theme/ccafs-sites/photos.php
@@ -31,15 +31,75 @@ $postType = "";
           $thumb = substr($filename, 0, $extension_pos) . '_q' . substr($filename, $extension_pos);
          ?>
 
-         <div class="site-photo <?php echo distance($sitepoint, $photopoint) ?>"> 
-            <a href="<?php the_permalink(); ?>">  <img height="60" class="site-photo" src="<?php echo $thumb  ?>" border="0">  </a>  
+         <div class="site-photo <?php echo distance($sitepoint, $photopoint) ?>">             
+            <a href="#" data-reveal-id="<?php echo $post->ID; ?>"><img height="60" class="site-photo" src="<?php echo $thumb; ?>" border="0"></a>
          </div>
 
       <?php } ?>
    <?php
    endwhile;
 // Reset Query
-   wp_reset_query();
-   ?><!-- end loop-->
-
+//   wp_reset_query();
+   ?><!-- end loop-->  
 </div>
+<?php while (have_posts()) : the_post(); ?>
+    <?php
+    $postType = $post->post_type; 
+    $photopoint = get_post_meta($post->ID, 'geoRSSPoint', true);
+    $metaDesc = get_post_meta($post->ID, 'content_description', true);
+    $srcID = get_post_meta($post->ID, 'syndication_feed_id', true);
+    if (strlen($metaDesc) > 250)
+      $metaDesc = substr($metaDesc, 0, 250) . "...";
+   
+    if (distance($sitepoint, $photopoint) < $rangephotos) {
+        $filename = get_post_meta($post->ID, 'galleryThumb', true);
+        $extension_pos = strrpos($filename, '.'); // find position of the last dot, so where the extension starts
+        $thumb = substr($filename, 0, $extension_pos) . '_q' . substr($filename, $extension_pos);
+       ?>
+
+       <div id="<?php echo $post->ID; ?>" class="reveal-modal" data-reveal style="top:100px">                       
+          <div class ="metaDesc">
+            <strong><h2 class="teasertitle"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2></strong>
+            <div class="entrymeta">Source: <em><?php echo get_bookmark($srcID)->link_description; ?></em> <a target="_blank" href="<?php echo get_post_meta($post->ID, 'syndication_permalink', true); ?>">permalink</a></div>
+             
+            <p><?php echo $metaDesc; ?></p>
+             
+            <?php
+            $args2 = array(
+                'public' => true,
+                '_builtin' => false
+            );
+            $excludeTaxonomies = array("cgmap-countries", "farming_systems");
+            $output = 'objects'; // or names
+            $operator = 'and'; // 'and' or 'or'
+            $taxonomies = get_taxonomies($args2, $output, $operator);
+            if ($taxonomies) {
+               asort($taxonomies);
+               foreach ($taxonomies as $taxonomy) {
+                  $getArgs = array(
+                      'orderby' => 'name'
+                  );
+                  $terms = wp_get_object_terms($post->ID, $taxonomy->name);
+                  $count = count($terms);
+                  if ($count > 0 && (!in_array($taxonomy->name, $excludeTaxonomies))) {
+                     echo '<h3 class="videolabels">' . $taxonomy->label . ': <span class="taxItems">';
+                     unset($termNames);
+                     foreach ($terms as $term) {
+                        $termNames[] = $term->name;
+                     }
+                     echo join(", ", $termNames) . '</span></h3>';
+                  }
+               }
+            }
+            ?>
+         </div>
+         <div class="video photoset">
+              <?php the_content(); ?>
+          </div>
+          <a class="close-reveal-modal">&#215;</a>
+       </div>
+
+    <?php } ?>
+ <?php
+ endwhile;
+ ?>
