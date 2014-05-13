a1d087ed34ab3298825e6623ae1f3697068215fc
camilor67 <camilor67@gmail.com>
2014-05-13 13:20:26 -0500

    Json ccafs site
    Ccafs sites section has a conection DB problem.

--- wp-content/themes/amkn_theme/ccafs-sites/json.php
+++ wp-content/themes/amkn_theme/ccafs-sites/json.php
@@ -2,6 +2,8 @@
 require('../../../../wp-blog-header.php'); 
 $bmsid = htmlentities($_GET["bmsid"]);
 $table= htmlentities($_GET["table"]);
+mysql_connect(DB_HOST, DB_USER, DB_PASSWORD) or die(mysql_error());
+mysql_select_db(DB_NAME);
 $sth = mysql_query("SELECT * FROM ".$table." WHERE bms_id='".$bmsid."'");
 $rows = array();
 while($r = mysql_fetch_assoc($sth)) {
