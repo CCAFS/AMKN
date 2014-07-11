ccba914957c85f40ec8469b7bd5340923175374b
camilo rodriguez <camilor67@gmail.com>
2014-07-11 11:30:46 -0500

    embed - size parameters

--- wp-content/themes/amkn_theme/ccafs-sites-embed.php
+++ wp-content/themes/amkn_theme/ccafs-sites-embed.php
@@ -2,15 +2,21 @@
 /*
 Template Name: CCAFS sites embed
 */
+$height = 420;
+$width = 600;
+if($_GET['height'])
+  $height = $_GET['height'];
+if($_GET['width'])
+  $width = $_GET['width'];
 ?>
 <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
 <script>  
   var map;
   var markerArray = {};
   function initialize() {
-    var myLatlng = new google.maps.LatLng(12.968888, 41.138147);
+    var myLatlng = new google.maps.LatLng(12.968888, 10.138147);
     var mapOptions = {
-      zoom: 3,
+      zoom: 2,
       center: myLatlng
     }
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
@@ -47,7 +53,7 @@ Template Name: CCAFS sites embed
           }
 //          $("div#"+results.features[i].id).addClass("ccafs_sites_selected").siblings().removeClass("ccafs_sites_selected");  
 //          $('#sites').scrollTo($("div#"+results.features[i].id), 500,{offset: {top:-135, left:0}});
-          var contentString = '<div id="content"><b>'+results.features[i].properties.title+' ['+results.features[i].properties.country+'] <br>CCAFS Region: '+results.features[i].properties.region+'</b></div>';
+          var contentString = '<div id="content"><b>'+results.features[i].properties.title+' ['+results.features[i].properties.country+'] </b><br>CCAFS Region: '+results.features[i].properties.region+'</div>';
           infowindow = new google.maps.InfoWindow({
               content: contentString
           });
@@ -79,4 +85,4 @@ Template Name: CCAFS sites embed
   }
   google.maps.event.addDomListener(window, 'load', initialize);
 </script>
-<div style="height: 420px; width: 600px;" id="map-canvas"></div>
+<div style="height: <?php echo $height?>px; width: <?php echo $width?>px;" id="map-canvas"></div>
