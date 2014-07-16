74f7f812c5a1d756396b4cddd73db20329a193b5
camilo rodriguez <camilor67@gmail.com>
2014-07-16 15:53:28 -0500

    pb test

--- wp-content/plugins/amkn_rules/map.js
+++ wp-content/plugins/amkn_rules/map.js
@@ -669,13 +669,12 @@ function processCsvData(url) {
                     totalSources[csvStore.getValue(item, typeField)] = {};
                 totalSources[csvStore.getValue(item, typeField)][csvStore.getValue(item, "CID")] = 1;
 //                totalSources[csvStore.getValue(item,typeField)] += 1;
-                setTimeout(function() {
-                    tmptotal++;
-//        console.log(tmptotal+'/'+totalGraphs);        
-                    progressbar.progressbar("option", {
-                        value: (Math.round((index / items.length) * 100))
-                    });
-                }, 80);
+//                setTimeout(function() {
+                console.log(index + '/' + items.length);
+                progressbar.progressbar("option", {
+                    value: (Math.round((index+1 / items.length) * 100))
+                });
+//                }, 80);
             });
             var end = new Date().getTime();
             var time = end - start;
